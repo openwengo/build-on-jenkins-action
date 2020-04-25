73aae2fca835fe3bb4a456ae3b032f27727ac5da
Olivier Schiavo <olivier.schiavo@wengo.com>
2020-04-25 18:40:14 +0000

    * (wip) build on jenkins

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ var request = require('request');
 try {
   // `who-to-greet` input defined in action metadata file
   const jenkinsUrl = core.getInput('jenkins-url');
-  console.log(`Github hook will be called on ${jenkinsUrl}`);
+  console.log(`Jenkins url is ${jenkinsUrl}`);
   const time = (new Date()).toTimeString();
   // parse jenkins url
   const parsedUrl = url.parse(jenkinsUrl) ;
   const webHookUrl = new URL('/github-webhook/', jenkinsUrl);
+  console.log(`Github hook will be called on ${webHookUrl}`);
+
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
@@ -19,13 +21,12 @@ try {
   var responseStatus = 0 ;
   request.post({ url: webHookUrl ,
             agentOptions: { rejectUnauthorized: false },
-            agent: agent, 
             body: payload,
             json: true,
             headers: { "X-GitHub-Event" : "push", "content-type": "application/json" }
           }, function (err, response, body) {
-          console.log(`The response statusCode is ${response.statusCode}`);
-          responseStatus = response.statusCode ;
+            console.log(`The response statusCode is ${response.statusCode}`);
+            responseStatus = response.statusCode ;
           }
   )
   core.setOutput("http_status", responseStatus);
